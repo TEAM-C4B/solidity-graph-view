a6c129c7ac662abefbd9f43e687133c4602d8495
JinWooLee <jwoo619@naver.com>
2018-10-12 23:31:01 +0900

    goodbye...

--- del_and_div.py
+++ del_and_div.py
@@ -8,30 +8,35 @@ def delete_comment(data):
 		for j in range(0,len(data[i])):
 			if(data[i].find(';') < data[i].find('/') and data[i].find(';') != -1):
 				data[i] = data[i][0:data[i].find('/')-1]
-				continue
+				break;
+
 			if(data[i][j] == ' '):
 				continue
-			if(del_flag):
-				if(data[i][j] == '*'):
-					if(data[i][j+1] == '/'):
-						del_num.append(i)
-						del_flag = False
-						break
-					else:
-						continue
-				del_num.append(i)
-				continue	
 
 			if(data[i][j] == '/'):
 				if(data[i][j+1] == '/'):
 					del_num.append(i)
 					break
 				if(data[i][j+1] == '*'):
+					if(data[i].find('*/') != -1):
+						del_num.append(i)
+						break
+
 					del_flag = True
 					del_num.append(i)
 					break
+
+			if(del_flag):
+				if(data[i].find('*/') != -1):
+					del_num.append(i)
+					del_flag = False
+					break
+
+				del_num.append(i)
+
 			else:
 				break
+
 	del del_num[0]
 	del_num = list(set(del_num))
 	del_num.reverse()
@@ -63,19 +68,34 @@ def relation(data):
 
 	for i in range(0,len(data)):
 		if(data[i].find('contract') != -1):
-			key = data[i]
+			if(key != '' and key2 != ''):
+				data_div[key][key2] = data_div[key][key2].replace('  ','')[:-5]
+
+			key = data[i].replace('{\n','').replace(' ','').replace('contract','contract_').replace('is','_')
 			key2 = ''
 			data_div[key] = {}
 			continue
 
 		elif(data[i].find('interface') != -1 or data[i].find('library') != -1 ):
+			if(key != '' and key2 != ''):
+				data_div[key][key2] = data_div[key][key2].replace('  ','')[:-3]
+
 			key = ''
 			key2 = ''
 			continue
 
 		elif(key != ''):
 			if(data[i].find("function") != -1):
-				key2 = data[i]
+				if(key2 != ''):
+					data_div[key][key2] = data_div[key][key2].replace('  ','')[:-3]
+
+				key2 = data[i].replace('{\n','').replace(' ','').replace('function','function_')
+				key2 = key2.replace('public','_public_').replace('internal','_internal_').replace('private','_private_').replace('external','_external_')
+				key2 = key2.replace('view','_view_').replace('pure','_pure_').replace('constant','_constant_')
+				key2 = key2.replace('__','_')
+				if(key2[-1:] == '_'):
+					key2 = key2[:-1]
+
 				data_div[key][key2] = ''
 				continue
 
@@ -83,3 +103,18 @@ def relation(data):
 			data_div[key][key2] += data[i]
 
 	return data_div
+
+def view(a):
+	for i in range(0,len(a.keys())):
+		print '\033[34m---------------contract--------------------------'
+		print "\033[35m"+a.keys()[i]+"\033[0m"
+		print '\033[34m---------------function--------------------------'
+		if(len(a[a.keys()[i]].keys()) == 0):
+			print '               NO FUNCTION'
+		for j in range(0,len(a[a.keys()[i]].keys())):
+			print '\033[31m'+a[a.keys()[i]].keys()[j]+"\033[0m" + '\n\033[33m'+a[a.keys()[i]][a[a.keys()[i]].keys()[j]]+"\033[0m\n"
+		print'\033[34m-------------------------------------------------\n\n\033[0m'
+
+delete_comment('flag.sol')
+beautifier("check.sol")
+view(relation("check.sol"))
